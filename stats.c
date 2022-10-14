9a593673837f2ede4ad0d69d96d285d591b462e9
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-10-14 10:58:57 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -6,6 +6,36 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.min = 0;
     s.max = 0;
     
+    float sum=0.0, avg=0.0, small=0.0, larger=0.0;
+    int i=0;
+    
+    for(i=0; i<setlength; i++)
+    {
+        sum=sum+numberset[i];
+    }
+    
+    avg=sum/setlength;
+    
+    small=numberset[0]
+    for(i=0; i<setlength; i++)
+    {
+        if(numberset[i]<small)
+        {
+             small=numberset[i];
+        }
+    }
+    
+    larger=numberset[0]
+    for(i=0; i<setlength; i++)
+    {
+        if(numberset[i]>larger)
+        {
+             larger=numberset[i];
+        }
+    }
+    
+    printf("AVG=%f, SMALL=%f, LARGER=%f", avg,small,larger);
+    
     
 }
 
