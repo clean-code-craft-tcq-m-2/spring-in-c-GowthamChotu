d183228781848e7a7bf5d920cf40a4dcda719f5d
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-10-18 00:00:16 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -40,7 +40,7 @@ TEST_CASE("raises alerts when max is greater than threshold") {
 
     const float maxThreshold = 10.2;
     
-    check_and_alert(maxThreshold, alerters[0], computedStats);
+    check_and_alert(maxThreshold, &(alerters[0]), computedStats);
     
 
     // need a way to check if both emailAlerter, ledAlerter were called
