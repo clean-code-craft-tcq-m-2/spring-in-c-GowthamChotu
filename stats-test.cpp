8a5934dbaf1e325da1449791bd169d7c7b558258
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-10-17 23:33:20 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -39,12 +39,9 @@ TEST_CASE("raises alerts when max is greater than threshold") {
     struct Stats computedStats = compute_statistics(numberset, setlength);
 
     const float maxThreshold = 10.2;
-    alerters[0]=&emailAlerter;
     
-    check_and_alert(maxThreshold, alerters[0], computedStats);
+    check_and_alert(maxThreshold, alerters, computedStats);
     
-    alerters[1]=&ledAlerter;
-    check_and_alert(maxThreshold, alerters[1], computedStats);
 
     // need a way to check if both emailAlerter, ledAlerter were called
     // you can define call-counters along with the functions, as shown below
